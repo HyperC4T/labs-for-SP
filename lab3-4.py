96321c67541ffb8b3a10d9862c6069119701e8cb
HyperC4T <115781737+HyperC4T@users.noreply.github.com>
2023-04-19 16:21:53 +0800

    Update and rename untitled.py to lab3-4.py

---
+++ lab3-4.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "http://biik.ru/rasp/cg109.htm"
+a = requests.get(URL)
+a.encoding="windows-1251"
+if a == 404:
+	print ('Not Found')
+else: 
+	all_web_code = a.text
+	bs =BeautifulSoup(all_web_code,"html.parser")
+	Lesson = bs.findAll('td')
+	
+	file = open ("Raspisanie.txt", "w")
+	for item in Lesson:
+		file.write(item.text +"\n")
+	file.close()
+
+	line = open ("Raspisanie.txt").readlines()
+	for i in range(104):
+		line.pop(0)
+	
+	f = open ("Raspisanie.txt", "w")
+	for i in line:
+		f.write(i)
+	f.close()
+fout = open ("Raspisanie.txt", "r")
+print(fout.read())
