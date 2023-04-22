0ee83a5362b23d3ca6823ad91f04307030106795
HyperC4T <115781737+HyperC4T@users.noreply.github.com>
2023-04-22 14:26:30 +0800

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -6,7 +6,7 @@ ftp.login()
 
 
 # Путь на нашем компьютере где сохранить файл.
-out = '/home/HYPERC4T/desktop/tree.json.gz'
+out = 'C:\\Users\\HYPERC4T\\Desktop\\tree.json.gz'
 
 with open(out, 'wb') as f:
     ftp.retrbinary('RETR ' + 'tree.json.gz', f.write)
